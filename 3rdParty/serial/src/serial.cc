ab39463e9c53b0048d18a24d93b5f1602c8225ff
Mauro Bellone <bellonemauro@gmail.com>
2021-10-16 12:18:22 +0200

    update serial lib

--- 3rdParty/serial/src/serial.cc
+++ 3rdParty/serial/src/serial.cc
@@ -132,7 +132,16 @@ Serial::read (std::vector<uint8_t> &buffer, size_t size)
 {
   ScopedReadLock lock(this->pimpl_);
   uint8_t *buffer_ = new uint8_t[size];
-  size_t bytes_read = this->pimpl_->read (buffer_, size);
+  size_t bytes_read = 0;
+
+  try {
+    bytes_read = this->pimpl_->read (buffer_, size);
+  }
+  catch (const std::exception &e) {
+    delete[] buffer_;
+    throw;
+  }
+
   buffer.insert (buffer.end (), buffer_, buffer_+bytes_read);
   delete[] buffer_;
   return bytes_read;
@@ -143,7 +152,14 @@ Serial::read (std::string &buffer, size_t size)
 {
   ScopedReadLock lock(this->pimpl_);
   uint8_t *buffer_ = new uint8_t[size];
-  size_t bytes_read = this->pimpl_->read (buffer_, size);
+  size_t bytes_read = 0;
+  try {
+    bytes_read = this->pimpl_->read (buffer_, size);
+  }
+  catch (const std::exception &e) {
+    delete[] buffer_;
+    throw;
+  }
   buffer.append (reinterpret_cast<const char*>(buffer_), bytes_read);
   delete[] buffer_;
   return bytes_read;
@@ -172,6 +188,7 @@ Serial::readline (string &buffer, size_t size, string eol)
     if (bytes_read == 0) {
       break; // Timeout occured on reading 1 byte
     }
+    if(read_so_far < eol_len) continue;
     if (string (reinterpret_cast<const char*>
          (buffer_ + read_so_far - eol_len), eol_len) == eol) {
       break; // EOL found
@@ -193,46 +210,46 @@ Serial::readline (size_t size, string eol)
 }
 
 vector<string>
-Serial::readlines(size_t size, string eol)
+Serial::readlines (size_t size, string eol)
 {
-	ScopedReadLock lock(this->pimpl_);
-	std::vector<std::string> lines;
-	size_t eol_len = eol.length();
-	uint8_t *buffer_ = static_cast<uint8_t*>
-		(alloca(size * sizeof(uint8_t)));
-	size_t read_so_far = 0;
-	size_t start_of_line = 0;
-	while (read_so_far < size) {
-		size_t bytes_read = this->read_(buffer_ + read_so_far, 1);
-		read_so_far += bytes_read;
-		if (bytes_read == 0) {
-			if (start_of_line != read_so_far) {
-				lines.push_back(
-					string(reinterpret_cast<const char*> (buffer_ + start_of_line),
-						read_so_far - start_of_line));
-			}
-			break; // Timeout occured on reading 1 byte
-		}
-		if (string(reinterpret_cast<const char*>
-			(buffer_ + read_so_far - eol_len), eol_len) == eol) {
-			// EOL found
-			lines.push_back(
-				string(reinterpret_cast<const char*> (buffer_ + start_of_line),
-					read_so_far - start_of_line));
-			start_of_line = read_so_far;
-		}
-		if (read_so_far == size) {
-			if (start_of_line != read_so_far) {
-				lines.push_back(
-					string(reinterpret_cast<const char*> (buffer_ + start_of_line),
-						read_so_far - start_of_line));
-			}
-			break; // Reached the maximum read length
-		}
-	}
-	return lines;
+  ScopedReadLock lock(this->pimpl_);
+  std::vector<std::string> lines;
+  size_t eol_len = eol.length ();
+  uint8_t *buffer_ = static_cast<uint8_t*>
+    (alloca (size * sizeof (uint8_t)));
+  size_t read_so_far = 0;
+  size_t start_of_line = 0;
+  while (read_so_far < size) {
+    size_t bytes_read = this->read_ (buffer_+read_so_far, 1);
+    read_so_far += bytes_read;
+    if (bytes_read == 0) {
+      if (start_of_line != read_so_far) {
+        lines.push_back (
+          string (reinterpret_cast<const char*> (buffer_ + start_of_line),
+            read_so_far - start_of_line));
+      }
+      break; // Timeout occured on reading 1 byte
+    }
+    if(read_so_far < eol_len) continue;
+    if (string (reinterpret_cast<const char*>
+         (buffer_ + read_so_far - eol_len), eol_len) == eol) {
+      // EOL found
+      lines.push_back(
+        string(reinterpret_cast<const char*> (buffer_ + start_of_line),
+          read_so_far - start_of_line));
+      start_of_line = read_so_far;
+    }
+    if (read_so_far == size) {
+      if (start_of_line != read_so_far) {
+        lines.push_back(
+          string(reinterpret_cast<const char*> (buffer_ + start_of_line),
+            read_so_far - start_of_line));
+      }
+      break; // Reached the maximum read length
+    }
+  }
+  return lines;
 }
-
 
 size_t
 Serial::write (const string &data)

e756234c82029dac2b8987f2557c4206df59a28c
Mauro Bellone <bellonemauro@gmail.com>
2021-11-26 09:23:29 +0100

    update

--- apps/BioZone6/src/solutions.cpp
+++ apps/BioZone6/src/solutions.cpp
@@ -133,9 +133,9 @@ void BioZone6_GUI::onPushButtonSolutionX(QPushButton *_button, int _idx)
 		currentProtocolFileName = m_operational_mode_protocol_path +
 			"ON_Button" + QString::number(_idx) + ".prt";
 		if (QFile::exists(currentProtocolFileName)) {
-			QMessageBox::warning(this, m_str_warning,
-				"FOUND" + tr("<br>%1")
-				.arg(QDir::toNativeSeparators(currentProtocolFileName)));
+			//QMessageBox::warning(this, m_str_warning,
+			//	"FOUND" + tr("<br>%1")
+			//	.arg(QDir::toNativeSeparators(currentProtocolFileName)));
 			this->runProtocolFile(currentProtocolFileName);
 		}
 		else
